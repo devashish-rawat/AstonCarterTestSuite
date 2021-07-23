09f2e24982797bc0bdcbb7f57c98d27fd7e98073
Devashish Rawat <dev.rawat7396@gmail.com>
2021-07-23 15:17:20 +0530

    homepage test

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+    @Test
+    public void homepageTest(){
+        System.out.println("homepageTest");
+    }
+}
